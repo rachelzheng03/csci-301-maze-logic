79737bd784756f15eb1ca8224bbc646c256384cf
RachelZheng <rzheng02@wm.edu>
2022-09-27 17:53:53 -0400

    pseudocode for testWallsInPerfectMaze

--- MazeByRachelZheng/test/generation/MazeFactoryTest.java
+++ MazeByRachelZheng/test/generation/MazeFactoryTest.java
@@ -18,7 +18,7 @@ class MazeFactoryTest {
 	
 	/** 
 	 * Tests if a maze only has one exit. The maze is correct if it only has one exit. If no exits or more than 1 exit, test fails. 
-	 * */
+	 */
 	@Test
 	public void testOneExit() {
 		//check every border wall and determine if it is an exit. checks for amount of exits. (maybe private method)
@@ -31,7 +31,7 @@ class MazeFactoryTest {
 	}
 
 	/**
-	 * Tests if there is a way out of the maze from no matter where you are in the maze.
+	 * Tests if there is a way out of the maze from no matter where you are in the maze. Correct if mazedists contains a valid distance value for every cell.
 	 */
 	@Test
 	public void testEveryCellHasPathToExit(){
@@ -41,7 +41,23 @@ class MazeFactoryTest {
 		//test for large width small height 
 		//test for larger maze (60x60)
 		//test incorrect case: maze with no exit
-		//test incorrect case: maze with blocked off cell
+		//test incorrect case: maze with closed off cell
+	}
+	/**
+	 * Tests if a perfect maze with n cells and no rooms has the correct amount of walls.
+	 */
+	@Test
+	public void testWallsInPerfectMaze() {
+		//make sure maze is perfect or create a perfect maze
+		//get maximum amount of internal walls a maze of size (nxm) can have
+		//get amount of cells
+		//get amount of internal walls in maze
+		//check that amount of internal walls in maze = maximum amount of internal walls-(amount of cells-1)
+		//test for smallest possible maze (4x4)
+		//test for small width large height 
+		//test for large width small height 
+		//test for larger maze (60x60)
+		//test incorrect case: maze with closed off cell
 	}
 	
 
