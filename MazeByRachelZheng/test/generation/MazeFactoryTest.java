9e662602776b14bfb99af561925424cbf0009adb
RachelZheng <rzheng02@wm.edu>
2022-09-27 16:38:26 -0400

    pseudocode for testEveryCellHasPathToExit

--- MazeByRachelZheng/test/generation/MazeFactoryTest.java
+++ MazeByRachelZheng/test/generation/MazeFactoryTest.java
@@ -23,13 +23,27 @@ class MazeFactoryTest {
 	public void testOneExit() {
 		//check every border wall and determine if it is an exit. checks for amount of exits. (maybe private method)
 		//test for smallest possible maze (4x4)
-		//test for small width large height (4x240)
-		//test for large width small height (300x4)
-		//test for largest possible maze (300x240)
+		//test for small width large height 
+		//test for large width small height 
+		//test for larger maze (60x60)
 		//test for incorrect case: maze with more than 1 exit
 		//test for incorrect case: maze with 0 exits
 	}
 
+	/**
+	 * Tests if there is a way out of the maze from no matter where you are in the maze.
+	 */
+	@Test
+	public void testEveryCellHasPathToExit(){
+		//check that every cell of mazedists has a value that is not INFINITY
+		//test for smallest possible maze (4x4)
+		//test for small width large height 
+		//test for large width small height 
+		//test for larger maze (60x60)
+		//test incorrect case: maze with no exit
+		//test incorrect case: maze with blocked off cell
+	}
+	
 
 
 }
