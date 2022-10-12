654d22fd91068c253aa0cac6b584fad6df6c9779
RachelZheng <rzheng02@wm.edu>
2022-10-12 00:52:02 -0400

    Test files for ReliableRobot, ReliableSensor, and Wizard created

---
+++ MazeByRachelZheng/test/gui/ReliableSensorTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package gui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author rzhe2
+ *
+ */
+class ReliableSensorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
